d930bdb5cdd0fd253bd18a41b6da74e4e8c0083e
Sergio Rodrigo Royo <sergio.rodrigo.royo@gmail.com>
2019-06-11 15:41:28 +0200

    connect to mongo on test

--- tests/integration/server.test.js
+++ tests/integration/server.test.js
@@ -1,9 +1,24 @@
 const request = require('supertest');
 const expect = require('chai').expect;
+const mongoose = require('mongoose');
 
 const app = require('../../server');
 
 describe('Users', () => {
+  before(() => {
+    mongoose.set('useNewUrlParser', true);
+    mongoose.connect('mongodb://localhost:27017/testbooksdb');
+    mongoose.connection
+      .once('open', () => console.log('Connected!'))
+      .on('error', (error) => {
+        console.warn('Error : ',error);
+      });
+  });
+
+  after(() => {
+    mongoose.disconnect();
+  });
+
   it('authenticates an existing user', done => {
     request(app)
       .post('/users/signin')
